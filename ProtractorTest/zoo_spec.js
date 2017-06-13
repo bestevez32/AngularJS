87d30cd7d9b414af35c9c97de16f2027232c00fc
Brandon Estevez <bestevez32@gmail.com>
2017-06-12 19:18:46 -0700

    Create zoo_spec.js
    This is the actual test file.

---
+++ ProtractorTest/zoo_spec.js
@@ -0,0 +1,10 @@
+describe ("text zoo site for input field on the home page", function(){
+	it ("to test input field and check text output", function(){
+		browser.get("http://www.thetestroom.com/jswebapp/");
+		element(by.model("person.name")).sendKeys("I will subscribe to this channel");
+		element(by.binding("person.name")).getText().then(function(text){
+			console.log(text);
+		});
+	});
+
+});
